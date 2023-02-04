d2e26e66253b601ec39286f0358b488b348d9181
David Arias <gedafu2005@gmail.com>
2023-02-04 12:08:58 -0500

    fix: clear db version

--- services/migrations/1675529859216_clients.mjs
+++ services/migrations/1675529859216_clients.mjs
@@ -4,7 +4,6 @@ import { Kysely } from "kysely";
  * @param db {Kysely<any>}
  */
 export async function up(db) {
-    await db.insertInto('clients').values({ name: 'Doug' }).values({ name: 'David' }).execute()
 }
 
 /**
