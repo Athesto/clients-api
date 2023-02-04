15e63ee481eef68933c6f5534d65f779a12f37b9
David Arias <gedafu2005@gmail.com>
2023-02-04 08:50:36 -0500

    feat: add db instance

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,14 +1,30 @@
-import { Api, StackContext } from '@serverless-stack/resources';
+import { Api as ApiGateway, RDS, StackContext } from '@serverless-stack/resources';
 
 export function MyStack({ stack }: StackContext) {
-  const api = new Api(stack, 'api', {
+  const cluster = new RDS(stack, 'Cluster', {
+    engine: 'postgresql11.13',
+    defaultDatabaseName: 'CounterDB',
+    migrations: 'services/migrations'
+  });
+
+  const api = new ApiGateway(stack, 'api', {
+    defaults: {
+      function: {
+        bind: [cluster]
+      }
+    },
     routes: {
       'GET /clients': 'functions/get-all-clients.handler',
       'GET /requests/{client_id}': 'functions/get-one-request.handler',
       'POST /requests/{client_id}': 'functions/increment-request.handler'
     }
   });
+
   stack.addOutputs({
-    ApiEndpoint: api.url
+    ApiEndpoint: api.url,
+    SecretArn: cluster.secretArn,
+    ClusterIdentifier: cluster.clusterIdentifier
   });
+
+  return api;
 }
--- stacks/index.ts
+++ stacks/index.ts
@@ -1,13 +1,13 @@
-import { MyStack } from "./MyStack";
-import { App } from "@serverless-stack/resources";
+import { App } from '@serverless-stack/resources';
+import { MyStack } from './MyStack';
 
-export default function (app: App) {
+export default function main(app: App) {
   app.setDefaultFunctionProps({
-    runtime: "nodejs16.x",
-    srcPath: "services",
+    runtime: 'nodejs16.x',
+    srcPath: 'services',
     bundle: {
-      format: "esm",
-    },
+      format: 'esm'
+    }
   });
   app.stack(MyStack);
 }
