db1c2582a9b2025af2ba06940e40ff11601cfe95
David Arias <gedafu2005@gmail.com>
2023-02-04 11:16:48 -0500

    feat: types from kysely

--- services/core/sql.generated.ts
+++ services/core/sql.generated.ts
@@ -1,14 +1,21 @@
-export interface Database {
-  clients: Clients;
-  requests: Requests;
-}
+import { ColumnType } from 'kysely';
+
+export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
+  ? ColumnType<S, I | undefined, U>
+  : ColumnType<T, T | undefined, T>;
 
 export interface Clients {
-  id: number;
+  id: Generated<number>;
   name: string;
 }
 
 export interface Requests {
-  client_id: number;
-  api_calls: number;
+  api_calls: number | null;
+  client_id: number | null;
+  id: Generated<number>;
+}
+
+export interface Database {
+  clients: Clients;
+  requests: Requests;
 }
