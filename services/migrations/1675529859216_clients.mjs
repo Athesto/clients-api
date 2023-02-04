2797961de7df2607556f95f7ad318b5cba2a28ae
David Arias <gedafu2005@gmail.com>
2023-02-04 12:01:28 -0500

    feat: add migration to start with 2 clients

--- services/migrations/1675529859216_clients.mjs
+++ services/migrations/1675529859216_clients.mjs
@@ -4,6 +4,7 @@ import { Kysely } from "kysely";
  * @param db {Kysely<any>}
  */
 export async function up(db) {
+    await db.insertInto('clients').values({ name: 'Doug' }).values({ name: 'David' }).execute()
 }
 
 /**
