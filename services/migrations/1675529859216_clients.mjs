c342052cbc450cd41d809c5780271892c71f4da6
David Arias <gedafu2005@gmail.com>
2023-02-04 12:30:41 -0500

    fix: add right migration

--- services/migrations/1675529859216_clients.mjs
+++ services/migrations/1675529859216_clients.mjs
@@ -4,6 +4,16 @@ import { Kysely } from "kysely";
  * @param db {Kysely<any>}
  */
 export async function up(db) {
+    await db.schema.createTable('clients')
+        .addColumn("id", 'serial', (col) => col.primaryKey())
+        .addColumn("name", "text", (col) => col.notNull())
+        .execute();
+    await db.schema.createTable('requests')
+        .addColumn("id", 'serial', (col) => col.primaryKey())
+        .addColumn("client_id", 'integer', (col) => col.references('clients').notNull())
+        .addColumn("api_calls", "integer", (col) => col.notNull())
+        .execute();
+    await db.insertInto('clients').values({ name: 'Doug' }, { name: 'David' }).execute()
 }
 
 /**
