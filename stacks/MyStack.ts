1f443706c38a31bba3293a7990ead49beee3d3c4
David Arias <gedafu2005@gmail.com>
2023-02-04 08:29:30 -0500

    reafactor: add endpoints

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,12 +1,14 @@
-import { StackContext, Api } from "@serverless-stack/resources";
+import { Api, StackContext } from '@serverless-stack/resources';
 
 export function MyStack({ stack }: StackContext) {
-  const api = new Api(stack, "api", {
+  const api = new Api(stack, 'api', {
     routes: {
-      "GET /": "functions/lambda.handler",
-    },
+      'GET /clients': 'functions/get-all-clients.handler',
+      'GET /requests/{client_id}': 'functions/get-one-request.handler',
+      'POST /requests/{client_id}': 'functions/increment-request.handler'
+    }
   });
   stack.addOutputs({
-    ApiEndpoint: api.url,
+    ApiEndpoint: api.url
   });
 }
